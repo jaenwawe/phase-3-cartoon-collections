8765177f35e64e3054eee56c4eb71b1b6a81f124
jaenwawe <nwawe.jae@gmail.com>
2021-09-21 21:01:55 -0500

    commit tree 01a0c0bcec96c8867ca831ff8a9d03d0ce9b442f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,45 @@
-def roll_call_dwarves# code an argument here
+
+dwarves = %w[
+  Doc
+  Dopey
+  Bashful
+  Grumpy
+  Sneezy
+  Sleepy
+  Happy
+]
+
+def roll_call_dwarves(dwarves)    # code an argument here
   # Your code here
+  drawves.each_with_index |dwarve, index|
+    puts drawve
+    "#{index}: #{drawve}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = %w[earth wind fire water heart]
+
+def summon_captain_planet planeteer_calls # code an argument here
+  value=[]
+
+ # planeteer_calls.map { |call| call.capitalize + '!' }
+
+
+  array.each do |item|
+      item.capitalize.join("!")
+      valuee<<item
+
+  end
 end
 
 def long_planeteer_calls# code an argument here
   # Your code here
 end
-
-def find_the_cheese# code an argument here
+potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
+def find_the_cheese    potentially_cheesy_items # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    return array.include?(cheese)
+  end
 end
